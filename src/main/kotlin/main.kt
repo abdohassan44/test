ff63ab04c401675df3a72d88fb8bb0efac7ec07a
abdelrahman <abdelrahman.hassan@yelloco.com>
2021-07-08 17:36:23 +0200

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,35 @@
+import sun.rmi.runtime.Log
+import java.io.File
+import java.io.IOException
+import java.util.concurrent.TimeUnit
+import javax.naming.Context
+
+
+fun main(args: Array<String>) {
+    exec("git rev-parse HEAD", stdIn = "yes")
+
+}
+fun exec(cmd: String, stdIn: String = "", captureOutput:Boolean = false, workingDir: File = File(".")): String? {
+    try {
+        val process = ProcessBuilder(*cmd.split("\\s".toRegex()).toTypedArray())
+            .directory(workingDir)
+            .redirectOutput(if (captureOutput) ProcessBuilder.Redirect.PIPE else ProcessBuilder.Redirect.INHERIT)
+            .redirectError(if (captureOutput) ProcessBuilder.Redirect.PIPE else ProcessBuilder.Redirect.INHERIT)
+            .start().apply {
+                if (stdIn != "") {
+                    outputStream.bufferedWriter().apply {
+                        write(stdIn)
+                        flush()
+                        close()
+                    }
+                }
+                waitFor(60, TimeUnit.SECONDS)
+            }
+        if (captureOutput) {
+            return process.inputStream.bufferedReader().readText()
+        }
+    } catch (e: IOException) {
+        e.printStackTrace()
+    }
+    return null
+}
